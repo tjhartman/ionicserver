44b4003b003f9c39e73b794dc815dc61ce81d27a
T.J. Hartman <tjhartman@TJs-MacBook-Pro.local>
2017-06-20 15:59:01 -0600

    first commit

---
+++ data.js
@@ -0,0 +1,464 @@
+[
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Nurturing',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Objective',
+            Value: 'White'
+        },
+        {
+            Option: 'Proactive',
+            Value: 'Red'
+        },
+        {
+            Option: 'Insightful',
+            Value: 'Yellow'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Arrogant',
+            Value: 'Red'
+        },
+        {
+            Option: 'Indesicive',
+            Value: 'White'
+        },
+        {
+            Option: 'A perfectionist',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Poor follow through',
+            Value: 'Yellow'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Enthusiastic',
+            Value: 'Yellow'
+        },
+        {
+            Option: 'Kind',
+            Value: 'White'
+        },
+        {
+            Option: 'Caring',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Prodctive',
+            Value: 'Red'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Relentless',
+            Value: 'Red'
+        },
+        {
+            Option: 'Suspicious',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Indifferent',
+            Value: 'White'
+        },
+        {
+            Option: 'Naive',
+            Value: 'Yellow'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Peaceful',
+            Value: 'White'
+        },
+        {
+            Option: 'Carefree',
+            Value: 'Yellow'
+        },
+        {
+            Option: 'Decisive',
+            Value: 'Red'
+        },
+        {
+            Option: 'Loyal',
+            Value: 'Blue'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Silently stubborn',
+            Value: 'White'
+        },
+        {
+            Option: 'Worry prone',
+            Value: 'Blue'
+        },
+        {
+            Option: 'An interuptor',
+            Value: 'Yellow'
+        },
+        {
+            Option: 'Obsessive',
+            Value: 'Red'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Sociable',
+            Value: 'Yellow'
+        },
+        {
+            Option: 'Assertive',
+            Value: 'Red'
+        },
+        {
+            Option: 'Intimate',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Non-discriminate',
+            Value: 'White'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Self-critical',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Bossy',
+            Value: 'Red'
+        },
+        {
+            Option: 'Unfocused',
+            Value: 'Yellow'
+        },
+        {
+            Option: 'Avoids conflict',
+            Value: 'White'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Flexible',
+            Value: 'Yellow'
+        },
+        {
+            Option: 'Voice of reason',
+            Value: 'White'
+        },
+        {
+            Option: 'Action-oriented',
+            Value: 'Red'
+        },
+        {
+            Option: 'Analytical',
+            Value: 'Blue'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Critical of others',
+            Value: 'Red'
+        },
+        {
+            Option: 'Disinterested',
+            Value: 'White'
+        },
+        {
+            Option: 'Overly sensitive',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Irresponsible',
+            Value: 'Yellow'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Determined',
+            Value: 'Red'
+        },
+        {
+            Option: 'Thoughtful',
+            Value: 'Blue'
+        },
+        {
+            Option: 'A good listener',
+            Value: 'White'
+        },
+        {
+            Option: 'Positive',
+            Value: 'Yellow'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Unmotivated',
+            Value: 'White'
+        },
+        {
+            Option: 'Vain',
+            Value: 'Yellow'
+        },
+        {
+            Option: 'Demanding',
+            Value: 'Red'
+        },
+        {
+            Option: 'Unforgiving',
+            Value: 'Blue'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Happy',
+            Value: 'Yellow'
+        },
+        {
+            Option: 'Compassionate',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Inventive',
+            Value: 'White'
+        },
+        {
+            Option: 'Responsible',
+            Value: 'Red'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Impulsive',
+            Value: 'Yellow'
+        },
+        {
+            Option: 'Impatient',
+            Value: 'Red'
+        },
+        {
+            Option: 'Moody',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Indirect Communicator',
+            Value: 'White'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'A leader',
+            Value: 'Red'
+        },
+        {
+            Option: 'Patient',
+            Value: 'White'
+        },
+        {
+            Option: 'Fun-loving',
+            Value: 'Yellow'
+        },
+        {
+            Option: 'Respectful',
+            Value: 'Blue'
+        }
+        ]
+    },
+    {
+        Question: 'I am...',
+        Options: [{
+            Option: 'Jealous',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Reluctant',
+            Value: 'White'
+        },
+        {
+            Option: 'Argumentative',
+            Value: 'Red'
+        },
+        {
+            Option: 'Obnoxious',
+            Value: 'Yellow'
+        }
+        ]
+    },
+    // Situations --------------------------------------------------------
+    {
+        Question: 'If I applied for a job, a prospective employer would most likely hire me because I am:',
+        Options: [{
+            Option: 'Driven, direct, and delegating',
+            Value: 'Red'
+        },
+        {
+            Option: 'Deliberate, accurate, and reliable',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Patient, adaptable, and tactful',
+            Value: 'White'
+        },
+        {
+            Option: 'Fun-loving, spirited, and casual',
+            Value: ''
+        }
+        ]
+    },
+    {
+        Question: 'When involved in an intimate relationship, if I feel threatened by my partner, I:',
+        Options: [{
+            Option: 'Cry, feel hurt, and plan revenge',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Distance myself to avoid further conflict',
+            Value: 'Yellow'
+        },
+        {
+            Option: 'Fight back with facts and anger',
+            Value: 'Red'
+        },
+        {
+            Option: 'Become quiet, withdrawn, and often hold anger until I blow up over some minor issue later',
+            Value: 'White'
+        }
+        ]
+    },
+    {
+        Question: 'For me life is most meaningful when it:',
+        Options: [{
+            Option: 'Allows me to be playful, lighthearted, and optimistic',
+            Value: 'Yellow'
+        },
+        {
+            Option: 'Is free of pressure and stress',
+            Value: 'White'
+        },
+        {
+            Option: 'Is filled with people and purpose',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Is task-oriented and productive',
+            Value: 'Red'
+        }
+        ]
+    },
+    {
+        Question: 'As a child, I was:',
+        Options: [{
+            Option: 'Stubborn, bright and/or aggressive',
+            Value: 'Red'
+        },
+        {
+            Option: 'Too talkative, happy, and/or playful',
+            Value: 'Yellow'
+        },
+        {
+            Option: 'Well behaved, caring, and/or depressed',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Quiet, easygoing, and/or shy',
+            Value: 'White'
+        }
+        ]
+    },
+    {
+        Question: 'As an adult, I am:',
+        Options: [{
+            Option: 'Accepting, content, and/or unmotivated',
+            Value: 'White'
+        },
+        {
+            Option: 'Opinionated, determined, and/or bossy',
+            Value: 'Red'
+        },
+        {
+            Option: 'Responsible, honest, and/or unforgiving',
+            Value: 'Blue'
+        },
+        {
+            Option: 'Charismatic, positive, and/or obnoxious',
+            Value: 'Yellow'
+        }
+        ]
+    },
+    {
+        Question: 'In an argument with a friend, I am most likely to be:',
+        Options: [{
+            Option: 'Verbally stubborn about facts',
+            Value: 'Red'
+        },
+        {
+            Option: "Concerned about others' feelings and principles",
+            Value: 'Blue'
+        },
+        {
+            Option: 'Silently stubborn, uncomfortable, and/or confused',
+            Value: 'White'
+        },
+        {
+            Option: 'Loud, uncomfortable, and/or compromising',
+            Value: 'Yellow'
+        }
+        ]
+    },
+    {
+        Question: 'Work is:',
+        Options: [{
+            Option: "A healthy activity, which should be done right if it's to be done at all. Work should be done before one plays",
+            Value: 'Blue'
+        },
+        {
+            Option: "A most productive way to spend one's time",
+            Value: 'Red'
+        },
+        {
+            Option: 'A necessary evil, much less inviting than play',
+            Value: 'Yellow'
+        },
+        {
+            Option: "A positive activity as long as it is something I enjoy and don't feel pressured to accomplish",
+            Value: 'White'
+        }
+        ]
+    },
+   
+]
