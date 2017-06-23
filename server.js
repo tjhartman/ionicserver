9ba4b5b279012406003cdbd9d3dd3bfee0b9587c
T.J. Hartman <tjhartman@TJs-MacBook-Pro.local>
2017-06-23 12:06:40 -0600

    slowly but surely

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const massive = require('massive');
 const app = express();
 const controller = require('./dbcontroller.js');
-const config = require('./config.js')
+// const config = require('./config.js')
 
 var port = 3000;
 
@@ -22,6 +22,6 @@ app.get('/api/getQuestionData', controller.getQuestionData)
 
 
 app.listen(process.env.PORT || port, function() {
-    console.log('listening on port', this.address().port, app.settings.env);
+    console.log('listening on port', this.address().port);
   });
 
